4194f7bbcc9841d3e139122280ecc8745c53b611
Amitshendge <Amitshendgepro@gmail.com>
2024-11-28 15:31:12 -0500

    just added all reset to deny for caution

--- streamlit_app/test.py
+++ streamlit_app/test.py
@@ -68,7 +68,7 @@ if 'messages' not in st.session_state:
 st.title("Form Filling Chatbot")
 st.write("Available forms now:\n1. Addendum Lease - K1384,\n2. Addendum Sale - K1117, \n3. Extension of Review Period for Resale Certificate - K1389, \n4. K1388 - Payment Plan Addendum")
 # User input text box
-user_message = st.text_input("Say something to the bot:", key="user_message", on_change=clean_text)
+user_message = st.text_input("Say Hi to the bot:", key="user_message", on_change=clean_text)
 
 
 # Display the conversation history
