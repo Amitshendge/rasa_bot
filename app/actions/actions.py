1e4de6665fee5a922a5109dfc3d0ad1a5a9c2a4d
Amitshendge <Amitshendgepro@gmail.com>
2024-11-28 13:50:42 -0500

    checking setslot

--- app/actions/actions.py
+++ app/actions/actions.py
@@ -48,13 +48,14 @@ class ActionAskDynamicQuestions(Action):
         print("state", state)
         # Get current index and questions
         current_index = state["current_index"]
+        print("current state",current_index)
         questions = list(state["questions"].keys())
 
         # Save the user's latest response if it's not the initial call
         if tracker.latest_message.get("text") and 0 < current_index <= len(questions):
             # Append the latest response to the responses list
             state["responses"][state['questions'][questions[current_index-1]]] = tracker.latest_message["text"]
-
+            SlotSet("response_list", state["responses"])
         # Check if there are more questions to ask
         if current_index < len(questions):
             # Ask the next question
@@ -68,12 +69,15 @@ class ActionAskDynamicQuestions(Action):
             with open(file_path, "w") as file:
                 json.dump(state, file)
             print("state", state)
+            print(tracker.get_slot("response_list"))
             return [ActiveLoop('action_ask_dynamic_questions'), SlotSet("response_list", state["responses"])]
         elif current_index == len(questions):
+            state["current_index"] += 1
             # Save the updated state back to the JSON file
             with open(file_path, "w") as file:
                 json.dump(state, file)
             print("final state", state)
+            print(tracker.get_slot("response_list"))
             return [ActiveLoop('action_ask_dynamic_questions'), SlotSet("response_list", state["responses"])]
         else:
             print("Else")
