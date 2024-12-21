484cd60fb0c9119e448c3262d2bb1ebf2734db22
Amitshendge <Amitshendgepro@gmail.com>
2024-12-21 22:46:02 +0530

    Updates complete code new update

--- app/actions/actions.py
+++ app/actions/actions.py
@@ -11,10 +11,12 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet, ActiveLoop, AllSlotsReset
+from rasa_sdk.events import SlotSet, ActiveLoop, AllSlotsReset, FollowupAction
 import json
 import os
 import difflib
+from actions.form_filling_code.pdf_form import PDFFormFiller
+
 
 class ActionAskDynamicQuestions(Action):
     def name(self) -> str:
@@ -27,18 +29,15 @@ class ActionAskDynamicQuestions(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
         # Path to the JSON file to store state
         form_name_final = tracker.get_slot("identified_form_name")
-        print(tracker.get_slot("response_list"))
-        print("form_name_final", form_name_final)
         response_list = tracker.get_slot("response_list")
         if response_list:
             state = response_list[0]
         else:
             state = {
-                "questions": self.read_json(f"/app/actions/form_feilds_mapping/{form_name_final}.json"),
+                "questions": self.read_json(f"/app/actions/form_feilds_mapping_v2/{form_name_final}.json"),
                 "current_index": 0,
                 "responses": {}
             }
-        print("state", state)
         # Get current index and questions
         current_index = state["current_index"]
         questions = list(state["questions"].keys())
@@ -46,28 +45,28 @@ class ActionAskDynamicQuestions(Action):
         # Save the user's latest response if it's not the initial call
         if tracker.latest_message.get("text") and current_index > 0:
             # Append the latest response to the responses list
-            state["responses"][state['questions'][questions[current_index-1]]] = tracker.latest_message["text"]
+            form_feild = PDFFormFiller().get_form_feild(state['questions'][questions[current_index-1]])
+            state["responses"] = PDFFormFiller().fill_response(state["responses"], form_feild, tracker.latest_message["text"])
         # Check if there are more questions to ask
         if current_index < len(questions):
             # Ask the next question
             next_question = questions[current_index]
+            extra_question = PDFFormFiller().get_extra_question(state['questions'][questions[current_index]])
             dispatcher.utter_message(text=next_question)
+            if extra_question:
+                dispatcher.utter_message(json_message={"type":"select_options","payload":[{"title": i} for i in extra_question]})
             # Increment the current index
             state["current_index"] += 1
             # Save the updated state back to the JSON file
-            print("state", state)
             return [ActiveLoop('action_ask_dynamic_questions'), SlotSet("response_list", state)]
         else:
-            print("Else")
-            print("state", state)
             # All questions have been asked
-            from actions.form_filling_code.pdf_form import PDFFormFiller
             pdf_path = f'/app/actions/form_feilds_NAVAR/{form_name_final}.pdf'
             output_path = f"/app/outputs/{form_name_final}_filled.pdf"
             feild_values = state["responses"]
             PDFFormFiller().fill_pdf(pdf_path, output_path, feild_values)
             dispatcher.utter_message(text="Thank you for answering all the questions!")
-            dispatcher.utter_message(json_message=f"{form_name_final}_filled.pdf")
+            dispatcher.utter_message(json_message={"type":"download_file","file_name":f"{form_name_final}_filled.pdf"})
             return [ActiveLoop(None),AllSlotsReset()]
 
 
@@ -83,19 +82,15 @@ class SayFormName(Action):
         :param form_names: List of form names to match against.
         :return: Closest form name.
         """
-        print("user_input", user_input)
-        print("form_names", form_names)
-        closest_match = difflib.get_close_matches(user_input, form_names, n=1, cutoff=0.1)
+        closest_match = difflib.get_close_matches(user_input, form_names, n=1, cutoff=0.5)
         return closest_match[0] if closest_match else None
     
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        print("form", tracker.slots)
         form_name = tracker.get_slot("form_name2")
         if form_name:
-            final_form_name = self.find_closest_form(form_name, [i[:-4] for i in list(os.listdir("/app/actions/form_feilds_NAVAR"))])
-            print(tracker.get_slot("identified_form_name"))
+            final_form_name = self.find_closest_form(form_name, [i[:-5] for i in list(os.listdir("/app/actions/form_feilds_mapping_v2"))])
             dispatcher.utter_message(text=f"Do you want to fill {final_form_name}")
         else:
             dispatcher.utter_message(text="I don't know which form you're filling out!")
@@ -110,3 +105,19 @@ class All_reset(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         return [AllSlotsReset()]
+    
+class ActionTriggerAction(Action):
+    def name(self) -> Text:
+        return "action_trigger_action"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Extract the parameter
+        param = tracker.get_slot('param')
+        dispatcher.utter_message(text=f"The parameter received is: {param}")
+        tracker.slots["identified_form_name"] = param
+        # Add your logic here
+        return [FollowupAction('action_ask_dynamic_questions'),
+                SlotSet("identified_form_name", param)]
--- streamlit_app/test.py
+++ streamlit_app/test.py
@@ -3,10 +3,14 @@ import requests
 import re
 import base64
 import os
+from functools import partial
+import json
 
 # URL of the Rasa API (assuming it's running locally on port 5005)
 RASA_URL = "http://localhost:2005/webhooks/rest/webhook"
-download_foler = "/home/amitshendgepro/rasa_bot/outputs"
+# download_foler = "/home/amitshendgepro/rasa_bot/outputs"
+download_foler = "/Users/amitshendge/Documents/rasa_bot/outputs"
+
 def generate_download_link(file_path, link_text="Download File"):
     """
     Generates a hyperlink to download any file.
@@ -31,6 +35,7 @@ def generate_download_link(file_path, link_text="Download File"):
 
 # Function to send user message to Rasa and get the response
 def get_rasa_response(user_message):
+    print("user_message", user_message)
     payload = {"message": user_message}
     response = requests.post(RASA_URL, json=payload)
     if response.status_code == 200:
@@ -39,34 +44,79 @@ def get_rasa_response(user_message):
     else:
         return [{"text": "Sorry, I couldn't get a response from the bot."}]
 
-def clean_text():
+def clean_text(default_message=None, store_message=True):
     user_message = st.session_state['user_message']
+    if default_message:
+        user_message = default_message
+    print("user_message", user_message)
     if user_message:
         # Append user's message to the session state
-        st.session_state.messages.append({"role": "user", "text": user_message.replace("-", " ")})
-
+        if store_message:
+            st.session_state.messages.append({"role": "user", "text": user_message})
         # Get response from Rasa
         bot_response = get_rasa_response(user_message)
-
+    print("bot_response", bot_response)
     # Append bot's response to the session state
     for message in bot_response:
         if 'image' in message:
             st.session_state.messages.append({"role": "bot", "image": message.get('image')})
         elif 'custom' in message:
-            download_link = generate_download_link(os.path.join(download_foler, message.get('custom')), link_text="Download File")
-            st.session_state.messages.append({"role": "bot", "download": download_link})
+            payload = message.get('custom')
+            if payload.get('type') == 'download_file':
+                download_link = generate_download_link(os.path.join(download_foler, payload.get('file_name')), link_text="Download File")
+                st.session_state.messages.append({"role": "bot", "download": download_link})
+            elif payload.get('type') == 'select_options':
+                st.session_state.messages.append({"role": "bot", "text": "Please select one of the following options:"})
+                for option in payload.get('payload'):
+                    st.session_state.messages.append({"role": "bot", "button": option.get('title')})
+                    st.session_state.buttons_message.append({"role": "bot", "button": option.get('title')})
         else:
             st.session_state.messages.append({"role": "bot", "text": message.get('text')})
-    st.session_state['user_message']=""
+    if not default_message:
+        st.session_state['user_message']=""
+
+def button_callback(m):
+    # st.session_state['user_message'] = message['button']
+    for i in st.session_state.buttons_message[:]:
+        st.session_state.messages.remove(i)
+        st.session_state.buttons_message.remove(i)
+    clean_text(m)
 
     
 # Initialize session state to store conversation history if it doesn't exist
 if 'messages' not in st.session_state:
     st.session_state.messages = []
+if 'buttons_message' not in st.session_state:
+    st.session_state.buttons_message = []
+if 'button_clicked' not in st.session_state:
+    st.session_state['button_clicked'] = False
+if 'files' not in st.session_state:
+    st.session_state.files = [i[:-5] for i in list(os.listdir("./app/actions/form_feilds_mapping_v2"))]
+if 'forms' not in st.session_state:
+    st.session_state.forms = json.load(open("./app/actions/form_filling_code/forms_subset.json"))
+
+# Step 1: Select category
 
 # Streamlit UI
 st.title("Form Filling Chatbot")
-st.write("Available forms now:\n1. Addendum Lease - K1384,\n2. Addendum Sale - K1117, \n3. Extension of Review Period for Resale Certificate - K1389, \n4. K1388 - Payment Plan Addendum")
+category = st.selectbox("Select Form Category", list(st.session_state.forms.keys()))
+
+if category:
+    selected_form = st.selectbox(
+        f"Select a form from {category}",
+        ["Select Form here"] + st.session_state.forms[category]
+    )
+# st.session_state['button_clicked'] = False
+# option = st.selectbox("Choose an option:", ["Select Form here"] + st.session_state.get('files'), key="option")
+def button_action():
+    if selected_form != "Select Form here":
+        print("option", f'/trigger_action{{"param": "{selected_form}"}}')
+        response = clean_text(default_message=f'/trigger_action{{"param": "{selected_form}"}}', store_message=False)
+st.button("Submit", on_click=button_action)
+# if option != "None":
+#     response = clean_text(option)
+
+# st.write("Available forms now:\n1. Addendum Lease - K1384,\n2. Addendum Sale - K1117, \n3. Extension of Review Period for Resale Certificate - K1389, \n4. K1388 - Payment Plan Addendum \n5. Disclosure of Brokerage Relationship - K1207")
 # User input text box
 user_message = st.text_input("Say Hi to the bot:", key="user_message", on_change=clean_text)
 
@@ -80,5 +130,7 @@ for message in st.session_state.messages[::-1]:
             st.image(message['image'], width=200)
         elif 'download' in message:
             st.write(message['download'], unsafe_allow_html=True)
+        elif 'button' in message:
+            st.button(message['button'], key=message['button'], on_click=partial(button_callback, message['button']))
         else:
             st.write(f"Bot: {message['text']}")
